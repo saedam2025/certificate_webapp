61f56d0d4929c7fb893d4311cc352172da430a21
saedam2025 <windows7@hanmail.net>
2025-09-17 11:27:46 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def payroll_upload_file_multi():
                     os.remove(path)
                 except Exception:
                     pass
-            return (result_html or "") + f'</div><br><a href="/{sender_key}" style="display:block; margin:16px auto 24px; width:fit-content; padding:8px 16px; background:#1f3c88; color:#fff; text-decoration:none; border-radius:5px;">발송 페이지로 가기</a><br><br><img src="https://www.saedam.org/img_sub/logo.gif" alt="로고" style="display:block; margin:0 auto; height:48px; width:auto;">'
+            return (result_html or "") + f'</div><br><a href="/{sender_key}" style="display:block; margin:16px auto 24px; width:fit-content; padding:8px 16px; background:#1f3c88; color:#fff; text-decoration:none; border-radius:5px;">발송 페이지로 가기</a>'
         else:
             return "엑셀 파일(.xlsx)만 업로드 가능합니다."
 
@@ -531,6 +531,7 @@ def process_excel_multi(sender_key, filepath):
     result_html += f"""
         </div>
       </div>
+      <br><img src="https://www.saedam.org/img_sub/logo.gif" alt="로고" style="display:block; margin:0 auto; height:48px; width:auto;"><br><br>
     </body>
     </html>
     """
